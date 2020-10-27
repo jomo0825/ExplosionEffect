3aa770202647a666b601c50f9d9e19229d3a9a01
jomo0825 <jomo0825@gmail.com>
2020-10-28 02:36:35 +0800

    Initial commit

---
+++ Assets/ParticleSpeedControlExample/ParticlePlaySpeedControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlaySpeedControl : MonoBehaviour
+{
+    public ParticleSystem particleSystemToBeControlled;
+    public float targetSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var main = particleSystemToBeControlled.main;
+        main.simulationSpeed = targetSpeed;
+    }
+}
